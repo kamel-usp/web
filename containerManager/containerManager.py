5642cd64327bce51e0db1a12fee7a8d3353b60bd
Nathan Luiz <nathanluiz33@gmail.com>
2023-09-09 21:32:54 -0300

    adicionando dependency injection

--- containerManager/containerManager.py
+++ containerManager/containerManager.py
@@ -1,18 +1,18 @@
 from PriorityQueue import PriorityQueue
 import time
 
-container_id = 0
-
-def createContainer():
-    global container_id
-    container_id += 1
-    return container_id - 1
-
-def deleteContainer(container_id):
-    return 0
+class dockerApi:
+    def __init__(self):
+        pass
+    def createContainer():
+        print ("NAO SOU MOCK")
+        pass
+    def deleteContainer(container_id):
+        pass
 
 class containerManager:
-    def __init__(self, lifetime):
+    def __init__(self, lifetime, docker_api = dockerApi()):
+        self.docker_api = docker_api
         self.container_lifetime = lifetime
         self.lifetime_pq = PriorityQueue()
         self.user_id_to_container_id = dict()
@@ -24,7 +24,7 @@ class containerManager:
         if self.user_id_to_container_id.get(user_id) != None:
             return self.user_id_to_container_id[user_id]
         else:
-            container_id = createContainer()
+            container_id = self.docker_api.createContainer()
             self.user_id_to_container_id[user_id] = container_id
 
             self.lifetime_pq.insert((container_id, user_id), time.time() + self.container_lifetime)
@@ -35,7 +35,7 @@ class containerManager:
             container_id, user_id = self.lifetime_pq.top()
             self.lifetime_pq.pop()
             self.user_id_to_container_id.pop(user_id)
-            deleteContainer(container_id)
+            self.docker_api.deleteContainer(container_id)
         
 
 
