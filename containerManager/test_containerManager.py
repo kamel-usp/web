ec086317e8476a0c846eae49478b029aa1d00b42
Nathan Luiz <nathanluiz33@gmail.com>
2023-09-13 14:24:14 -0300

    ajeitando ident

--- containerManager/test_containerManager.py
+++ containerManager/test_containerManager.py
@@ -2,36 +2,36 @@ from containerManager import containerManager
 import time
 
 class mockDocker:
-    def __init__(self):
-        self.container_id = 0
-        
-    def createContainer(self):
-        self.container_id += 1
-        return self.container_id - 1
-    
-    def deleteContainer(self, container_id):
-        pass
+	def __init__(self):
+		self.container_id = 0
+		
+	def createContainer(self):
+		self.container_id += 1
+		return self.container_id - 1
+	
+	def deleteContainer(self, container_id):
+		pass
 
 def test_containerManager():
-    containers = containerManager (0.09, mockDocker())
-    list_id = []
+	containers = containerManager (0.09, mockDocker())
+	list_id = []
 
-    for i in range (5):
-        list_id.append(containers.getContainer (i))
-    for i in range (5):
-        assert containers.getContainer (i) == list_id[i]
+	for i in range (5):
+		list_id.append(containers.getContainer (i))
+	for i in range (5):
+		assert containers.getContainer (i) == list_id[i]
 
-    time.sleep (0.1)
+	time.sleep (0.1)
 
-    for i in range (5, 10):
-        list_id.append(containers.getContainer (i))
+	for i in range (5, 10):
+		list_id.append(containers.getContainer (i))
 
-    assert containers.activeContainerCount () == 10
-    containers.pruneContainers ()
-    assert containers.activeContainerCount () == 5
+	assert containers.activeContainerCount () == 10
+	containers.pruneContainers ()
+	assert containers.activeContainerCount () == 5
 
 def main():
-    test_containerManager ()
+	test_containerManager ()
 
 if __name__ == "__main__":
-    main()
+	main()
