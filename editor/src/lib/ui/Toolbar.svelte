bfffa80588937ca2b86570d0a81dafee2f58692b
Nathan Luiz <nathanluiz33@gmail.com>
2023-09-17 20:45:20 -0300

    ajeitando pequenos highlights

--- editor/src/lib/ui/Toolbar.svelte
+++ editor/src/lib/ui/Toolbar.svelte
@@ -1,32 +1,38 @@
 <script>
-  import { HomeOutline, EnvelopeOutline } from 'flowbite-svelte-icons';
-  import { Textarea, Toolbar, ToolbarGroup, ToolbarButton, Button } from 'flowbite-svelte';
-  import { PlayOutline } from 'flowbite-svelte-icons';
-  import { editor } from '$lib/stores/editor.ts';
-  import { get } from 'svelte/store';  
-  import { Spinner } from 'flowbite-svelte';
+  import { HomeOutline, EnvelopeOutline } from "flowbite-svelte-icons";
+  import {
+    Textarea,
+    Toolbar,
+    ToolbarGroup,
+    ToolbarButton,
+    Button,
+  } from "flowbite-svelte";
+  import { PlayOutline } from "flowbite-svelte-icons";
+  import { editor } from "$lib/stores/editor";
+  import { get } from "svelte/store";
+  import { Spinner } from "flowbite-svelte";
 
   let submitting = false;
-  
+
   async function submit() {
     submitting = true;
     const code = get(editor);
-      const response = await fetch('/api/containermanager/run', {
-        method: 'POST',
-        body: JSON.stringify({ code }),
-        headers: {
-          'content-type': 'application/json'
-        }
-      });
-      console.log(response.status);
-      let res = await response.json();
-      console.log(res);
-      submitting = false;
+    const response = await fetch("/api/containermanager/run", {
+      method: "POST",
+      body: JSON.stringify({ code }),
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    console.log(response.status);
+    let res = await response.json();
+    console.log(res);
+    submitting = false;
   }
 </script>
 
 <Toolbar>
-<!--   <ToolbarGroup>
+  <!--   <ToolbarGroup>
     <ToolbarButton name="Attach file"><PaperClipOutline class="w-5 h-5 rotate-45" /></ToolbarButton>
     <ToolbarButton name="Embed map"><MapPinAltSolid class="w-5 h-5" /></ToolbarButton>
     <ToolbarButton name="Upload image"><ImageOutline class="w-5 h-5" /></ToolbarButton>
--- editor/src/routes/+page.svelte
+++ editor/src/routes/+page.svelte
@@ -1,40 +1,51 @@
 <script lang="ts">
-	import { SplitPane } from '@rich_harris/svelte-split-pane';
+	import { SplitPane } from "@rich_harris/svelte-split-pane";
 	import CodeMirror from "svelte-codemirror-editor";
 	import { python } from "@codemirror/lang-python";
 	import { oneDark } from "@codemirror/theme-one-dark";
-	import { Button } from 'flowbite-svelte';
-	import { Tabs, TabItem } from 'flowbite-svelte';
-	import { ArrowRightOutline } from 'flowbite-svelte-icons';
+	import { Button } from "flowbite-svelte";
+	import { Tabs, TabItem } from "flowbite-svelte";
+	import { ArrowRightOutline } from "flowbite-svelte-icons";
 
 	import Toolbar from "$lib/ui/Toolbar.svelte";
 	import Status from "$lib/ui/Status.svelte";
 	import Terminal from "$lib/ui/Terminal.svelte";
-	import { editor } from '$lib/stores/editor.ts';
-	const dividerThickness = '20px';
+	import { editor } from "$lib/stores/editor";
+	const dividerThickness = "20px";
 </script>
 
-<SplitPane type="horizontal" min="100px" pos="70%" --thickness={dividerThickness}>
+<SplitPane
+	type="horizontal"
+	min="100px"
+	pos="70%"
+	--thickness={dividerThickness}
+>
 	<section slot="a">
 		<Tabs>
-		  <TabItem open>
-		    <span slot="title">HelloWorld.dpasp</span>
-		    <div id="codeMirror"><CodeMirror bind:value={$editor} lang={python()} theme={oneDark} /></div>
-		  </TabItem>
+			<TabItem open>
+				<span slot="title">HelloWorld.dpasp</span>
+				<div id="codeMirror">
+					<CodeMirror
+						bind:value={$editor}
+						lang={python()}
+						theme={oneDark}
+					/>
+				</div>
+			</TabItem>
 		</Tabs>
-		<Toolbar/>
+		<Toolbar />
 	</section>
 	<section slot="b">
 		<SplitPane type="vertical">
-				<section slot="a" id="output"><Status /></section>
-				<section slot="b" id="console"><Terminal /></section>
+			<section slot="a" id="output"><Status /></section>
+			<section slot="b" id="console"><Terminal /></section>
 		</SplitPane>
 	</section>
 </SplitPane>
 
 <style>
 	#codeMirror {
-		background-color: #242424; 
+		background-color: #242424;
 		min-height: 500px;
 	}
 </style>
