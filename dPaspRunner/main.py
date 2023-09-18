f7e6397ef64d4853fdfa5a1b7e7867a24b98340a
Nathan Luiz <nathanluiz33@gmail.com>
2023-09-18 02:17:42 -0300

    ajeitando CORS e retornando exec(code)

--- dPaspRunner/main.py
+++ dPaspRunner/main.py
@@ -1,8 +1,51 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import sys
+
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+]
 
-@app.get("/")
-def hello_world():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # You can specify the HTTP methods you want to allow
+    allow_headers=["*"],  # You can specify the HTTP headers you want to allow
+)
+
+
+class ListStream:
+    def __init__(self):
+        self.data = []
+
+    def write(self, s):
+        self.data.append(s)
+
+    def flush(self):
+        str1 = "".join(self.data)
+        return str1
+
+
+class CodeInput(BaseModel):
+    code: str
+
+
+@app.post("/api")
+def run_container(code_input: CodeInput):
+    code = code_input.code
+
+    sys.stdout = new_stdout = ListStream()
+
+    exec(code)
+
+    sys.stdout = sys.__stdout__
+
+    result = new_stdout.flush()
+    print(result)
+
+    return {"result": result}
--- editor/src/routes/api/containermanager/run/+server.js
+++ editor/src/routes/api/containermanager/run/+server.js
@@ -7,7 +7,32 @@ function delay(time) {
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request, cookies }) {
 	const { code } = await request.json();
-	// Mandar para o container manager
-	await delay(1000); // Simula a demora
-	return json('Ok');
+
+	// return json({ code });
+
+	try {
+		const response = await fetch(
+		  "http://localhost:8000/api",
+		  {
+			method: "POST",
+			body: JSON.stringify({ code }),
+			headers: {
+			  "Content-Type": "application/json", // Use "Content-Type" with a capital 'C'
+			},
+		  }
+		);
+  
+		if (!response.ok) {
+		  throw new Error(`HTTP error! Status: ${response.status}`);
+		}
+  
+		console.log(response.status);
+		let code_response = await response.json();
+		console.log(code_response);
+		
+		return json(code_response);
+	} catch (error) {
+		console.error("Error:", error);
+		return json({ code: error });
+	}
 }
