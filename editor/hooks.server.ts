263acc5dcfc364bfb5e3dd1cabfc8f61f890fe0d
Nathan Luiz <nathanluiz33@gmail.com>
2023-09-27 16:22:02 -0300

    tentando fazer login com github

---
+++ editor/hooks.server.ts
@@ -0,0 +1,12 @@
+import { SvelteKitAuth } from "@auth/sveltekit"
+import GitHub from "@auth/core/providers/github"
+import type { Handle } from "@sveltejs/kit";
+
+export const handle = SvelteKitAuth(async (event) => {
+  const authOptions = {
+    providers: [GitHub({ clientId: event.platform.env.GITHUB_ID, clientSecret: event.platform.env.GITHUB_SECRET })]
+    secret: event.platform.env.AUTH_SECRET,
+    trustHost: true
+  }
+  return authOptions
+}) satisfies Handle;
--- editor/src/routes/+page.svelte
+++ editor/src/routes/+page.svelte
@@ -12,6 +12,9 @@
 	import Terminal from "$lib/ui/Terminal.svelte";
 	import { editor } from "$lib/stores/editor";
 
+	import { signIn, signOut } from "@auth/sveltekit/client"
+  	import { page } from "$app/stores"
+
 	const dividerThickness = "20px";
 </script>
 
@@ -50,3 +53,23 @@
 		min-height: 500px;
 	}
 </style>
+
+<h1>SvelteKit Auth Example</h1>
+<p>
+  {#if $page.data.session}
+    {#if $page.data.session.user?.image}
+      <span
+        style="background-image: url('{$page.data.session.user.image}')"
+        class="avatar"
+      />
+    {/if}
+    <span class="signedInText">
+      <small>Signed in as</small><br />
+      <strong>{$page.data.session.user?.name ?? "User"}</strong>
+    </span>
+    <button on:click={() => signOut()} class="button">Sign out</button>
+  {:else}
+    <span class="notSignedInText">You are not signed in</span>
+    <button on:click={() => signIn("github")}>Sign In with GitHub</button>
+  {/if}
+</p>
