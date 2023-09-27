7ab17e4b372aaa88b0467a65531eef56f7f64400
Nathan Luiz <nathanluiz33@gmail.com>
2023-09-27 14:15:05 -0300

    construindo imagem toda vez

--- backend/main.py
+++ backend/main.py
@@ -6,19 +6,21 @@ day = 60 * 60 * 24
 
 cm = containerManager(2 * day)
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # startup code
-
+    cm.docker_api.assert_image_is_built()
     yield
     # shutdown code
     cm.stopAllContainers()
 
+
 app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/container_for_user/{user_id}")
 def get_container_for_user(user_id: int):
     print(f"Requesting container for user_id: {user_id}")
     id = cm.getContainer(user_id)
     return {"id": id}
-
