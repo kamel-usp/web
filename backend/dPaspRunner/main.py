670f93e88136a96f6de3c68230bd2fea2f4640b0
Maximilian Cabrajac Göritz <mcgoeritz@gmail.com>
2023-11-12 11:17:09 -0300

    Código que administra blobs dentro da instancia do container

--- backend/dPaspRunner/main.py
+++ backend/dPaspRunner/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, UploadFile
 from pydantic import BaseModel
 import pasp
 
@@ -23,7 +24,7 @@ class RunRequest(BaseModel):
 
 
 @app.post("/run")
-def run_code(run_req: RunRequest):
+async def run_code(run_req: RunRequest):
     sem = run_req.sem
     psem = run_req.psem
     code = run_req.code
@@ -38,3 +39,30 @@ def run_code(run_req: RunRequest):
     result = out.flush()
 
     return {"result": result}
+
+
+blob_folder = "~/blobs/"
+
+@app.post("/blob_upload")
+async def upload_blob(src: UploadFile):
+    chunk_size = 512
+    with open(f"{blob_folder}{src.filename}", "wb") as dest:
+        for chunk in iter(lambda: await src.read(chunk_size), b''):
+            dest.write(chunk)
+    return {"status": "ok"}
+
+@app.post("/blob_list")
+async def list_blobs():
+    return {"files" : os.listdir(blob_folder)}
+
+@app.post("/blob_delete")
+async def delete_blob(filename: str):
+    if os.remove(f"{blob_folder}{filename}"):
+        return {"status": "ok"}
+    else:
+        return {"status": "There is no file with this filename"}
+
+
+
+
+
