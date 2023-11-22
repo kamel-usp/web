1018030a3a68529f4032cc40419fc3472d33feee
Nathan Luiz <nathanluiz33@gmail.com>
2023-11-22 14:26:43 -0300

    task dpasp

--- backend/dPaspRunner/dpasp_api.py
+++ backend/dPaspRunner/dpasp_api.py
@@ -8,5 +8,10 @@ def run_program (sem, psem, code):
     if is_mock:
         return "Mock Output"
     else:
+        # TODO: return stdout and stderr
+        # TODO: pasp.approx.aseo(P, 100, psemantics = ...)
+
+        # program.Q
+        # https://github.com/kamel-usp/dpasp/blob/master/pasp/program.py
         P = pasp.parse(code, from_str=True, semantics=sem)
         return pasp.exact(P, psemantics=psem)
